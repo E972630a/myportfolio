31c5c014453581242ce8d195847021adf5df9779
Esau Aguilar Osuna <esauaguilar160@gmail.com>
2020-07-15 16:45:09 -0400

    responsiveness

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -8,7 +8,7 @@ class Contact extends Component {
                     <div className="container">
                         <h1 className="l-heading"><span className="text-primary">Contact</span> Me</h1>
                         <p className="l-p">Please fill out the form for any feedback</p>
-                        <form action="mailto:esauaguilarosuna@gmail.com" method="post" enctype="text/plain">
+                        <form action="mailto:esauaguilarosuna@gmail.com" method="post" enctype="text/plain" name="emailform">
                             <div className="form-group">
                                 <label for="name">Full Name:</label>
                                 <input type="text" name="name" id="name" />
@@ -20,12 +20,11 @@ class Contact extends Component {
                             <div className="form-group">
                                 <label for="message">Message or Questions:</label>
                                 <textarea name="message" id="message"></textarea>
-                                <button type="submit" className="btn">Submit</button> <br />
+                                <input type="submit" value="Submit" className="btn" />
                             </div>
                         </form>
                     </div>
                 </section>
-
             </div>
         )
     }
