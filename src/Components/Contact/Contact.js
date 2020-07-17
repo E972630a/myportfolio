c3e54a3d3e459593232a2467fe6972a95bca5f0b
Esau Aguilar Osuna <esauaguilar160@gmail.com>
2020-07-17 00:32:55 -0400

    new resume and required contact

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -11,16 +11,16 @@ class Contact extends Component {
                         <form action="mailto:esauaguilarosuna@gmail.com" method="post" enctype="text/plain" name="emailform">
                             <div className="form-group">
                                 <label for="name">Full Name:</label>
-                                <input type="text" name="name" id="name" />
+                                <input type="text" name="name" id="name" required/>
                             </div>
                             <div className="form-group">
                                 <label for="email">Email Address:</label>
-                                <input type="text" name="email" id="email" />
+                                <input type="text" name="email" id="email" required/>
                             </div>
                             <div className="form-group">
                                 <label for="message">Message or Questions:</label>
                                 <textarea name="message" id="message"></textarea>
-                                <input type="submit" value="Submit" className="btn" />
+                                <input type="submit" value="Submit" className="btn" required/>
                             </div>
                         </form>
                     </div>
