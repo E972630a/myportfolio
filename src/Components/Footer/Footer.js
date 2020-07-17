0b5619b4e2bd58e975b38959be37df7183ce35c0
Esau Aguilar Osuna <esauaguilar160@gmail.com>
2020-07-17 16:37:18 -0400

    adding the last updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -11,7 +11,9 @@ class Footer extends Component {
                             <li><a href="https://www.linkedin.com/in/esau-aguilar-osuna/" target="_blank" rel="noopener noreferrer"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
                             <li><a href="https://github.com/E972630a" target="_blank" rel="noopener noreferrer"><i class="fa fa-github fa-2x"></i></a></li>
                         </ul>
+                        <p className="last__update">* last updated 7/17/2020</p>
                     </div>
+                    
                 </footer>
             </div>
         )
